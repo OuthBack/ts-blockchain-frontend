a6148c25469c60242bb26c400de36bc34b4c6fc0
Henrique <riquessan@gmail.com>
2022-04-06 22:57:37 -0300

    Changes to run in my own backend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,32 +1,42 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 class App extends Component {
   state = { walletInfo: {} };
 
   componentDidMount() {
-    fetch(`${document.location.origin}/api/wallet-info`)
-      .then(response => response.json())
-      .then(json => this.setState({ walletInfo: json }));
+    const walletInfo = this.state.walletInfo.address;
+
+    walletInfo
+      ? fetch(`${process.env.API_URL}/api/wallet-info/${walletInfo.address}`)
+          .then((response) => response.json())
+          .then((json) => this.setState({ walletInfo: json }))
+      : fetch(`${process.env.API_URL}/api/wallet-info`)
+          .then((response) => response.json())
+          .then((json) => this.setState({ walletInfo: json }));
   }
 
   render() {
     const { address, balance } = this.state.walletInfo;
 
     return (
-      <div className='App'>
-        <img className='logo' src={logo}></img>
+      <div className="App">
+        <img className="logo" src={logo}></img>
+        <br />
+        <div>Welcome to the blockchain...</div>
         <br />
         <div>
-          Welcome to the blockchain...
+          <Link to="/blocks">Blocks</Link>
+        </div>
+        <div>
+          <Link to="/conduct-transaction">Conduct a Transaction</Link>
+        </div>
+        <div>
+          <Link to="/transaction-pool">Transaction Pool</Link>
         </div>
         <br />
-        <div><Link to='/blocks'>Blocks</Link></div>
-        <div><Link to='/conduct-transaction'>Conduct a Transaction</Link></div>
-        <div><Link to='/transaction-pool'>Transaction Pool</Link></div>
-        <br />
-        <div className='WalletInfo'>
+        <div className="WalletInfo">
           <div>Address: {address}</div>
           <div>Balance: {balance}</div>
         </div>
--- client/src/components/TransactionPool.js
+++ client/src/components/TransactionPool.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
-import Transaction from './Transaction';
-import { Link } from 'react-router-dom';
-import history from '../history';
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import Transaction from "./Transaction";
+import { Link } from "react-router-dom";
+import history from "../history";
 
 const POLL_INERVAL_MS = 10000;
 
@@ -10,22 +10,21 @@ class TransactionPool extends Component {
   state = { transactionPoolMap: {} };
 
   fetchTransactionPoolMap = () => {
-    fetch(`${document.location.origin}/api/transaction-pool-map`)
-      .then(response => response.json())
-      .then(json => this.setState({ transactionPoolMap: json }));
-  }
+    fetch(`${process.env.API_URL}/api/transaction-pool-map`)
+      .then((response) => response.json())
+      .then((json) => this.setState({ transactionPoolMap: json }));
+  };
 
   fetchMineTransactions = () => {
-    fetch(`${document.location.origin}/api/mine-transactions`)
-      .then(response => {
-        if (response.status === 200) {
-          alert('success');
-          history.push('/blocks');
-        } else {
-          alert('The mine-transactions block request did not complete.');
-        }
-      });
-  }
+    fetch(`${process.env.API_URL}/api/mine-transactions`).then((response) => {
+      if (response.status === 200) {
+        alert("success");
+        history.push("/blocks");
+      } else {
+        alert("The mine-transactions block request did not complete.");
+      }
+    });
+  };
 
   componentDidMount() {
     this.fetchTransactionPoolMap();
@@ -42,28 +41,25 @@ class TransactionPool extends Component {
 
   render() {
     return (
-      <div className='TransactionPool'>
-        <div><Link to='/'>Home</Link></div>
+      <div className="TransactionPool">
+        <div>
+          <Link to="/">Home</Link>
+        </div>
         <h3>Transaction Pool</h3>
-        {
-          Object.values(this.state.transactionPoolMap).map(transaction => {
-            return (
-              <div key={transaction.id}>
-                <hr />
-                <Transaction transaction={transaction} />
-              </div>
-            )
-          })
-        }
+        {Object.values(this.state.transactionPoolMap).map((transaction) => {
+          return (
+            <div key={transaction.id}>
+              <hr />
+              <Transaction transaction={transaction} />
+            </div>
+          );
+        })}
         <hr />
-        <Button
-          bsStyle="danger"
-          onClick={this.fetchMineTransactions}
-        >
+        <Button bsStyle="danger" onClick={this.fetchMineTransactions}>
           Mine the Transactions
         </Button>
       </div>
-    )
+    );
   }
 }
 
